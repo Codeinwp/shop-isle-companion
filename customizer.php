6302c8b72ee086253f5cea126c5e0b2269e56f53
coti <stefan@themeisle.com>
2018-03-06 23:52:28 +0200

    Revert "About Page Template - Selective Refresh"
    This reverts commit 8d804f57c4aee7db791df657c18d68c7fe0a274e.

--- customizer.php
+++ customizer.php
@@ -9,9 +9,6 @@
  * @param $wp_customize
  */
 function shop_isle_companion_customize_register($wp_customize) {
-
-	$selective_refresh = isset( $wp_customize->selective_refresh ) ? 'postMessage' : 'refresh';
-
 	/**
 	 * Class ShopIsle_Companion_Aboutus_Page_Instructions
 	 */
@@ -124,7 +121,7 @@ function shop_isle_companion_customize_register($wp_customize) {
 	$wp_customize->add_setting( 'shop_isle_our_team_title', array(
 		'sanitize_callback' => 'shop_isle_sanitize_text',
 		'default'           => __( 'Meet our team', 'shop-isle' ),
-		'transport'         => $selective_refresh,
+		'transport'         => 'postMessage'
 	) );
 	$wp_customize->add_control( 'shop_isle_our_team_title', array(
 		'label'           => __( 'Title', 'shop-isle' ),
@@ -136,7 +133,7 @@ function shop_isle_companion_customize_register($wp_customize) {
 	$wp_customize->add_setting( 'shop_isle_our_team_subtitle', array(
 		'sanitize_callback' => 'shop_isle_sanitize_text',
 		'default'           => __( 'An awesome way to introduce the members of your team.', 'shop-isle' ),
-		'transport'         => $selective_refresh,
+		'transport'         => 'postMessage'
 	) );
 	$wp_customize->add_control( 'shop_isle_our_team_subtitle', array(
 		'label'           => __( 'Subtitle', 'shop-isle' ),
@@ -147,7 +144,6 @@ function shop_isle_companion_customize_register($wp_customize) {
 	/* Team members */
 	$wp_customize->add_setting( 'shop_isle_team_members', array(
 		'sanitize_callback' => 'shop_isle_sanitize_repeater',
-		'transport'         => $selective_refresh,
 		'default'           => json_encode( array(
 			array(
 				'image_url'   => get_template_directory_uri() . '/assets/images/team1.jpg',
@@ -222,7 +218,7 @@ function shop_isle_companion_customize_register($wp_customize) {
 	$wp_customize->add_setting( 'shop_isle_about_page_video_title', array(
 		'sanitize_callback' => 'shop_isle_sanitize_text',
 		'default'           => __( 'Presentation', 'shop-isle' ),
-		'transport'         => $selective_refresh,
+		'transport'         => 'postMessage'
 	) );
 	$wp_customize->add_control( 'shop_isle_about_page_video_title', array(
 		'label'           => __( 'Title', 'shop-isle' ),
@@ -234,7 +230,7 @@ function shop_isle_companion_customize_register($wp_customize) {
 	$wp_customize->add_setting( 'shop_isle_about_page_video_subtitle', array(
 		'sanitize_callback' => 'shop_isle_sanitize_text',
 		'default'           => __( 'What the video about our new products', 'shop-isle' ),
-		'transport'         => $selective_refresh,
+		'transport'         => 'postMessage'
 	) );
 	$wp_customize->add_control( 'shop_isle_about_page_video_subtitle', array(
 		'label'           => __( 'Subtitle', 'shop-isle' ),
@@ -281,7 +277,7 @@ function shop_isle_companion_customize_register($wp_customize) {
 	$wp_customize->add_setting( 'shop_isle_our_advantages_title', array(
 		'sanitize_callback' => 'shop_isle_sanitize_text',
 		'default'           => __( 'Our advantages', 'shop-isle' ),
-		'transport'         => $selective_refresh,
+		'transport'         => 'postMessage'
 	) );
 	$wp_customize->add_control( 'shop_isle_our_advantages_title', array(
 		'label'           => __( 'Title', 'shop-isle' ),
@@ -292,7 +288,6 @@ function shop_isle_companion_customize_register($wp_customize) {
 	/* Advantages */
 	$wp_customize->add_setting( 'shop_isle_advantages', array(
 		'sanitize_callback' => 'shop_isle_sanitize_repeater',
-		'transport'         => $selective_refresh,
 		'default'           => json_encode( array(
 			array(
 				'icon_value' => 'icon_lightbulb',
@@ -360,288 +355,4 @@ function shop_isle_companion_is_aboutus_page() {
  */
 function shop_isle_companion_is_not_aboutus_page() {
     return ! is_page_template( 'template-about.php' );
-}
-
-
-/**
- * Add selective refresh for About page section controls
- *
- * @param WP_Customize_Manager $wp_customize Theme Customizer object.
- */
-function shop_isle_about_page_register_partials( $wp_customize ) {
-	// Abort if selective refresh is not available.
-	if ( ! isset( $wp_customize->selective_refresh ) ) {
-		return;
-	}
-
-	/* Our team */
-	$wp_customize->selective_refresh->add_partial(
-		'shop_isle_our_team_title', array(
-			'selector'            => '.page-template-template-about .meet-out-team-title',
-			'render_callback'     => 'shop_isle_about_page_team_title_callback',
-			'container_inclusive' => false,
-		)
-	);
-
-	$wp_customize->selective_refresh->add_partial(
-		'shop_isle_our_team_subtitle', array(
-			'selector'            => '.page-template-template-about .meet-out-team-subtitle',
-			'render_callback'     => 'shop_isle_about_page_team_subtitle_callback',
-			'container_inclusive' => false,
-		)
-	);
-
-	$wp_customize->selective_refresh->add_partial(
-		'shop_isle_team_members', array(
-			'selector'            => '.page-template-template-about .about-team-member',
-			'render_callback'     => 'shop_isle_about_page_display_team_members',
-			'container_inclusive' => true,
-		)
-	);
-
-	/* Video */
-	$wp_customize->selective_refresh->add_partial(
-		'shop_isle_about_page_video_title', array(
-			'selector'            => '.page-template-template-about .video-box .video-title',
-			'render_callback'     => 'shop_isle_about_page_video_title_callback',
-			'container_inclusive' => false,
-		)
-	);
-
-	$wp_customize->selective_refresh->add_partial(
-		'shop_isle_about_page_video_subtitle', array(
-			'selector'            => '.page-template-template-about .video-box .video-subtitle',
-			'render_callback'     => 'shop_isle_about_page_video_subtitle_callback',
-			'container_inclusive' => false,
-		)
-	);
-
-	/* Our advantages */
-	$wp_customize->selective_refresh->add_partial(
-		'shop_isle_our_advantages_title', array(
-			'selector'            => '.page-template-template-about .module-title.our_advantages',
-			'render_callback'     => 'shop_isle_about_page_advantages_title_callback',
-			'container_inclusive' => false,
-		)
-	);
-
-	$wp_customize->selective_refresh->add_partial(
-		'shop_isle_advantages', array(
-			'selector'            => '.page-template-template-about .module-advantages .multi-columns-row',
-			'render_callback'     => 'shop_isle_about_page_display_advantages',
-			'container_inclusive' => true,
-		)
-	);
-}
-
-add_action( 'customize_register', 'shop_isle_about_page_register_partials' );
-
-/**
- * Callback function for the about page, team section title
- *
- * @return string - team title value
- */
-function shop_isle_about_page_team_title_callback() {
-	return get_theme_mod( 'shop_isle_our_team_title' );
-}
-
-/**
- * Callback function for the about page, team section subtitle
- *
- * @return string - team subtitle value
- */
-function shop_isle_about_page_team_subtitle_callback() {
-	return get_theme_mod( 'shop_isle_our_team_subtitle' );
-}
-
-/**
- * Callback function for team members on about page
- */
-function shop_isle_about_page_display_team_members() {
-
-	$shop_isle_team_members = get_theme_mod(
-		'shop_isle_team_members', json_encode(
-			array(
-				array(
-					'image_url'   => get_template_directory_uri() . '/assets/images/team1.jpg',
-					'text'        => 'Eva Bean',
-					'subtext'     => 'Developer',
-					'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit lacus, a iaculis diam.',
-				),
-				array(
-					'image_url'   => get_template_directory_uri() . '/assets/images/team2.jpg',
-					'text'        => 'Maria Woods',
-					'subtext'     => 'Designer',
-					'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit lacus, a iaculis diam.',
-				),
-				array(
-					'image_url'   => get_template_directory_uri() . '/assets/images/team3.jpg',
-					'text'        => 'Booby Stone',
-					'subtext'     => 'Director',
-					'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit lacus, a iaculis diam.',
-				),
-				array(
-					'image_url'   => get_template_directory_uri() . '/assets/images/team4.jpg',
-					'text'        => 'Anna Neaga',
-					'subtext'     => 'Art Director',
-					'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit lacus, a iaculis diam.',
-				),
-			)
-		)
-	);
-
-	if ( ! empty( $shop_isle_team_members ) ) {
-		$shop_isle_team_members_decoded = json_decode( $shop_isle_team_members );
-		if ( ! empty( $shop_isle_team_members_decoded ) ) {
-
-			echo '<div class="hero-slider about-team-member">';
-			echo '<ul class="slides">';
-			foreach ( $shop_isle_team_members_decoded as $shop_isle_team_member ) {
-				$image_url   = ! empty( $shop_isle_team_member->image_url ) ? apply_filters( 'shop_isle_translate_single_string', $shop_isle_team_member->image_url, 'Team section' ) : '';
-				$text        = ! empty( $shop_isle_team_member->text ) ? apply_filters( 'shop_isle_translate_single_string', $shop_isle_team_member->text, 'Team section' ) : '';
-				$description = ! empty( $shop_isle_team_member->description ) ? apply_filters( 'shop_isle_translate_single_string', $shop_isle_team_member->description, 'Team section' ) : '';
-				$subtext     = ! empty( $shop_isle_team_member->subtext ) ? apply_filters( 'shop_isle_translate_single_string', $shop_isle_team_member->subtext, 'Team section' ) : '';
-
-				echo '<div class="col-sm-6 col-md-3 mb-sm-20 fadeInUp">';
-				echo '<div class="team-item">';
-
-				echo '<div class="team-image">';
-				if ( ! empty( $image_url ) ) {
-					if ( ! empty( $text ) ) {
-						echo '<img src="' . esc_url( $image_url ) . '" alt="' . esc_html( $text ) . '">';
-					} else {
-						echo '<img src="' . esc_url( $image_url ) . '" alt="">';
-					}
-				}
-				if ( ! empty( $description ) ) {
-					echo '<div class="team-detail">';
-					echo '<p class="font-serif">' . wp_kses_post( $description ) . '</p>';
-					echo '</div><!-- .team-detail -->';
-				}
-				echo '</div><!-- .team-image -->';
-
-				echo '<div class="team-descr font-alt">';
-				if ( ! empty( $text ) ) {
-					echo '<div class="team-name">' . wp_kses_post( $text ) . '</div>';
-				}
-				if ( ! empty( $subtext ) ) {
-					echo '<div class="team-role">' . wp_kses_post( $subtext ) . '</div>';
-				}
-				echo '</div><!-- .team-descr -->';
-
-				echo '</div><!-- .team-item -->';
-				echo '</div><!-- .col-sm-6 col-md-3 mb-sm-20 fadeInUp -->';
-			}
-			echo '</ul>';
-			echo '</div>';
-		}
-	} else {
-		if ( is_customize_preview() ) {
-			echo '<div class="hero-slider about-team-member"></div>';
-		}
-	}
-}
-
-/**
- * Callback function for video title on about page
- *
- * @return string - video title value
- */
-function shop_isle_about_page_video_title_callback() {
-	return get_theme_mod( 'shop_isle_about_page_video_title' );
-}
-
-/**
- * Callback function for video subtitle on about page
- *
- * @return string - video subtitle value
- */
-function shop_isle_about_page_video_subtitle_callback() {
-	return get_theme_mod( 'shop_isle_about_page_video_subtitle' );
-}
-
-/**
- * Callback function for advantages title
- *
- * @return string - advantages title value
- */
-function shop_isle_about_page_advantages_title_callback() {
-	return get_theme_mod( 'shop_isle_our_advantages_title' );
-}
-
-/**
- * Callback function for advantages boxes on about page
- */
-function shop_isle_about_page_display_advantages() {
-
-	$shop_isle_advantages = get_theme_mod(
-		'shop_isle_advantages', json_encode(
-			array(
-				array(
-					'icon_value' => 'icon_lightbulb',
-					'text'       => __( 'Ideas and concepts', 'shop-isle' ),
-					'subtext'    => __( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'shop-isle' ),
-				),
-				array(
-					'icon_value' => 'icon_tools',
-					'text'       => __( 'Designs & interfaces', 'shop-isle' ),
-					'subtext'    => __( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'shop-isle' ),
-				),
-				array(
-					'icon_value' => 'icon_cogs',
-					'text'       => __( 'Highly customizable', 'shop-isle' ),
-					'subtext'    => __( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'shop-isle' ),
-				),
-				array(
-					'icon_value' => 'icon_like',
-					'text'       => __( 'Easy to use', 'shop-isle' ),
-					'subtext'    => __( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', 'shop-isle' ),
-				),
-			)
-		)
-	);
-
-	if ( ! empty( $shop_isle_advantages ) ) :
-
-		$shop_isle_advantages_decoded = json_decode( $shop_isle_advantages );
-
-		if ( ! empty( $shop_isle_advantages_decoded ) ) :
-
-			echo '<div class="row multi-columns-row">';
-
-			foreach ( $shop_isle_advantages_decoded as $shop_isle_advantage ) :
-
-				$icon_value = ! empty( $shop_isle_advantage->icon_value ) ? apply_filters( 'shop_isle_translate_single_string', $shop_isle_advantage->icon_value, 'Advantages section' ) : '';
-				$text       = ! empty( $shop_isle_advantage->text ) ? apply_filters( 'shop_isle_translate_single_string', $shop_isle_advantage->text, 'Advantages section' ) : '';
-				$subtext    = ! empty( $shop_isle_advantage->subtext ) ? apply_filters( 'shop_isle_translate_single_string', $shop_isle_advantage->subtext, 'Advantages section' ) : '';
-
-				echo '<div class="col-sm-6 col-md-3 col-lg-3">';
-				echo '<div class="features-item">';
-
-				if ( ! empty( $icon_value ) ) :
-					echo '<div class="features-icon">';
-					echo '<span class="' . esc_attr( $icon_value ) . '"></span>';
-					echo '</div>';
-				endif;
-
-				if ( ! empty( $text ) ) :
-					echo '<h3 class="features-title font-alt">' . wp_kses_post( $text ) . '</h3>';
-				endif;
-
-				if ( ! empty( $subtext ) ) :
-					echo wp_kses_post( $subtext );
-				endif;
-
-				echo '</div>';
-				echo '</div>';
-
-			endforeach;
-
-			echo '</div>';
-		endif;
-	else :
-		if ( is_customize_preview() ) {
-			echo '<div class="row multi-columns-row"></div>';
-		}
-	endif;
 }
