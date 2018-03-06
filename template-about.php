8d804f57c4aee7db791df657c18d68c7fe0a274e
coti <stefan@themeisle.com>
2018-03-06 23:31:52 +0200

    About Page Template - Selective Refresh

--- template-about.php
+++ template-about.php
@@ -100,84 +100,11 @@ get_header(); ?>
 					echo '</div><!-- .row -->';
 				}
 
-				$shop_isle_team_members = get_theme_mod(
-					'shop_isle_team_members',json_encode(
-						array(
-							array(
-								'image_url' => get_template_directory_uri() . '/assets/images/team1.jpg',
-								'text' => 'Eva Bean',
-								'subtext' => 'Developer',
-								'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit lacus, a iaculis diam.',
-							),
-							array(
-								'image_url' => get_template_directory_uri() . '/assets/images/team2.jpg',
-								'text' => 'Maria Woods',
-								'subtext' => 'Designer',
-								'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit lacus, a iaculis diam.',
-							),
-							array(
-								'image_url' => get_template_directory_uri() . '/assets/images/team3.jpg',
-								'text' => 'Booby Stone',
-								'subtext' => 'Director',
-								'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit lacus, a iaculis diam.',
-							),
-							array(
-								'image_url' => get_template_directory_uri() . '/assets/images/team4.jpg',
-								'text' => 'Anna Neaga',
-								'subtext' => 'Art Director',
-								'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit lacus, a iaculis diam.',
-							),
-						)
-					)
-				);
-
-				if ( ! empty( $shop_isle_team_members ) ) {
-					$shop_isle_team_members_decoded = json_decode( $shop_isle_team_members );
-					if ( ! empty( $shop_isle_team_members_decoded ) ) {
-						echo '<div class="row">';
-						echo '<div class="hero-slider about-team-member">';
-						echo '<ul class="slides">';
-						foreach ( $shop_isle_team_members_decoded as $shop_isle_team_member ) {
-							$image_url = ! empty( $shop_isle_team_member->image_url ) ? apply_filters( 'shop_isle_translate_single_string', $shop_isle_team_member->image_url, 'Team section' ) : '';
-							$text = ! empty( $shop_isle_team_member->text ) ? apply_filters( 'shop_isle_translate_single_string', $shop_isle_team_member->text, 'Team section' ) : '';
-							$description = ! empty( $shop_isle_team_member->description ) ? apply_filters( 'shop_isle_translate_single_string', $shop_isle_team_member->description, 'Team section' ) : '';
-							$subtext = ! empty( $shop_isle_team_member->subtext ) ? apply_filters( 'shop_isle_translate_single_string', $shop_isle_team_member->subtext, 'Team section' ) : '';
-
-							echo '<div class="col-sm-6 col-md-3 mb-sm-20 fadeInUp">';
-							echo '<div class="team-item">';
-
-							echo '<div class="team-image">';
-							if ( ! empty( $image_url ) ) {
-								if ( ! empty( $text ) ) {
-									echo '<img src="' . esc_url( $image_url ) . '" alt="' . esc_html( $text ) . '">';
-								} else {
-									echo '<img src="' . esc_url( $image_url ) . '" alt="">';
-								}
-							}
-							if ( ! empty( $description ) ) {
-								echo '<div class="team-detail">';
-								echo '<p class="font-serif">' . wp_kses_post( $description ) . '</p>';
-								echo '</div><!-- .team-detail -->';
-							}
-							echo '</div><!-- .team-image -->';
-
-							echo '<div class="team-descr font-alt">';
-							if ( ! empty( $text ) ) {
-								echo '<div class="team-name">' . wp_kses_post( $text ) . '</div>';
-							}
-							if ( ! empty( $subtext ) ) {
-								echo '<div class="team-role">' . wp_kses_post( $subtext ) . '</div>';
-							}
-							echo '</div><!-- .team-descr -->';
-
-							echo '</div><!-- .team-item -->';
-							echo '</div><!-- .col-sm-6 col-md-3 mb-sm-20 fadeInUp -->';
-						}
-						echo '</ul>';
-						echo '</div>';
-						echo '</div>';
-					}
-				}
+				/* Team members start */
+				echo '<div class="row">';
+				shop_isle_about_page_display_team_members();
+				echo '</div>';
+				/* Team members end */
 				?>
 			</div>
 		</section>
@@ -241,73 +168,7 @@ get_header(); ?>
 					echo '</div>';
 				endif;
 
-				$shop_isle_advantages = get_theme_mod(
-					'shop_isle_advantages',json_encode(
-						array(
-							array(
-								'icon_value' => 'icon_lightbulb',
-								'text' => __( 'Ideas and concepts','shop-isle' ),
-								'subtext' => __( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.','shop-isle' ),
-							),
-							array(
-								'icon_value' => 'icon_tools',
-								'text' => __( 'Designs & interfaces','shop-isle' ),
-								'subtext' => __( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.','shop-isle' ),
-							),
-							array(
-								'icon_value' => 'icon_cogs',
-								'text' => __( 'Highly customizable','shop-isle' ),
-								'subtext' => __( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.','shop-isle' ),
-							),
-							array(
-								'icon_value' => 'icon_like',
-								'text' => __( 'Easy to use','shop-isle' ),
-								'subtext' => __( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.','shop-isle' ),
-							),
-						)
-					)
-				);
-
-				if ( ! empty( $shop_isle_advantages ) ) :
-
-					$shop_isle_advantages_decoded = json_decode( $shop_isle_advantages );
-
-					if ( ! empty( $shop_isle_advantages_decoded ) ) :
-
-						echo '<div class="row multi-columns-row">';
-
-						foreach ( $shop_isle_advantages_decoded as $shop_isle_advantage ) :
-
-							$icon_value = ! empty( $shop_isle_advantage->icon_value ) ? apply_filters( 'shop_isle_translate_single_string', $shop_isle_advantage->icon_value, 'Advantages section' ) : '';
-							$text = ! empty( $shop_isle_advantage->text ) ? apply_filters( 'shop_isle_translate_single_string', $shop_isle_advantage->text, 'Advantages section' ) : '';
-							$subtext = ! empty( $shop_isle_advantage->subtext ) ? apply_filters( 'shop_isle_translate_single_string', $shop_isle_advantage->subtext, 'Advantages section' ) : '';
-
-							echo '<div class="col-sm-6 col-md-3 col-lg-3">';
-							echo '<div class="features-item">';
-
-
-							if ( ! empty( $icon_value ) ) :
-								echo '<div class="features-icon">';
-								echo '<span class="' . esc_attr( $icon_value ) . '"></span>';
-								echo '</div>';
-								endif;
-
-							if ( ! empty( $text ) ) :
-								echo '<h3 class="features-title font-alt">' . wp_kses_post( $text ) . '</h3>';
-								endif;
-
-							if ( ! empty( $subtext ) ) :
-								echo wp_kses_post( $subtext );
-								endif;
-
-							echo '</div>';
-							echo '</div>';
-
-							endforeach;
-
-						echo '</div>';
-					endif;
-				endif;
+				shop_isle_about_page_display_advantages();
 				?>
 			</div><!-- .container -->
 		</section>
